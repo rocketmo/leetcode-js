2ab53434bd0461c7b51b8e39e751febe620d0021
Nien Van <nienvan12@gmail.com>
2021-07-05 18:14:36 -0400

    Solve problem 1 (two sum)

---
+++ solutions/1-two-sum.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+ var twoSum = function(nums, target) {
+    const numSet = new Map();
+
+    for (let i = 0; i < nums.length; i += 1) {
+        const diff = target - nums[i];
+        
+        if (numSet.has(diff)) {
+            return [numSet.get(diff), i];
+        }
+
+        numSet.set(nums[i], i);
+    }
+
+    return [-1, -1];
+};
