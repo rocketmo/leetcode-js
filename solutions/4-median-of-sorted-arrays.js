3c31de5acf71189c34fb121f2a75374584af379d
Nien Van <nienvan12@gmail.com>
2021-07-06 13:35:37 -0400

    Improved solution to problem 4

--- solutions/4-median-of-sorted-arrays.js
+++ solutions/4-median-of-sorted-arrays.js
@@ -4,15 +4,62 @@
  * @return {number}
  */
  var findMedianSortedArrays = function(nums1, nums2) {
-    const merged = [...nums1, ...nums2];
-    merged.sort((a, b) => a - b);
+    const shortArr = nums1.length > nums2.length ? nums2 : nums1;
+    const longArr = nums1.length > nums2.length ? nums1 : nums2;
 
-    if (merged.length % 2 === 1) {
-        return merged[(merged.length - 1) / 2];
+    // Shorter array may have zero elements; in that case, take the median from the longer array
+    if (shortArr.length === 0) {
+        if (longArr.length % 2 === 1) {
+            return longArr[(longArr.length - 1) / 2];
+        }
+
+        const median1 = longArr[longArr.length / 2];
+        const median2 = longArr[(longArr.length / 2) - 1];
+
+        return (median1 + median2) / 2;
     }
 
-    const median1 = merged[merged.length / 2];
-    const median2 = merged[(merged.length / 2) - 1];
+    // Make splits in the arrays until the median is found
+    let iMin = 0; // Min. index of shortArr
+    let iMax = shortArr.length; // Max. index of shortArr
+    let halfIndex = Math.floor((shortArr.length + longArr.length + 1) / 2);
 
-    return (median1 + median2) / 2;
+    while (iMin <= iMax) {
+        let i = Math.floor((iMin + iMax) / 2); // Current split index of shortArr
+        let j = halfIndex - i; // Current split index of longArr
+
+        // i is too small; increase it
+        if (i < shortArr.length && longArr[j - 1] > shortArr[i]) {
+            iMin = i + 1;
+        }
+
+        // i is too large; decrease it
+        else if (i > 0 && shortArr[i - 1] > longArr[j]) {
+            iMax = i - 1;
+        }
+
+        // Found correct i; return the median
+        else {
+            let maxOfLeft = 0;
+            if (i === 0) { maxOfLeft = longArr[j - 1]; }
+            else if (j === 0) { maxOfLeft = shortArr[i - 1]; }
+            else { maxOfLeft = Math.max(shortArr[i - 1], longArr[j - 1]); }
+
+            // Odd number of integers
+            if ((shortArr.length + longArr.length) % 2 === 1) {
+                return maxOfLeft;
+            }
+
+            let minOfRight = 0;
+            if (i === shortArr.length) { minOfRight = longArr[j]; }
+            else if (j === longArr.length) { minOfRight = shortArr[i]; }
+            else { minOfRight = Math.min(shortArr[i], longArr[j]); }
+
+            // Even number of integers
+            return (maxOfLeft + minOfRight) / 2;
+        }
+    }
+
+    // Should not reach here
+    return null;
 };
