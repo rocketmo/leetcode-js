100dd058b9334d4fc8f21037f0ffb5a5ca443b83
Nien Van <nienvan12@gmail.com>
2021-07-06 18:22:45 -0400

    Solve problem 1338

---
+++ solutions/1338-reduce-array-size-to-half.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+ var minSetSize = function(arr) {
+
+    // Map integer to number of occurrences
+    const counters = new Map();
+
+    for (let i = 0; i < arr.length; i += 1) {
+        if (!counters.has(arr[i])) {
+            counters.set(arr[i], 0);
+        }
+
+        counters.set(arr[i], counters.get(arr[i]) + 1);
+    }
+
+    // Convert counters into an array and sort from greatest to lowest
+    const counts = Array.from(counters.values());
+    counts.sort((a, b) => b - a);
+
+    let removalsRequired = 0;
+    let totalRemoved = 0;
+    const half = arr.length / 2;
+
+    for (let j = 0; j < counts.length; j += 1) {
+        removalsRequired += 1;
+        totalRemoved += counts[j];
+
+        if (totalRemoved >= half) {
+            return removalsRequired;
+        }
+    }
+
+    return removalsRequired;
+};
