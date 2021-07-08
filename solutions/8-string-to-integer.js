50c54f831622de7627454f37f65c81029693a609
Nien Van <nienvan12@gmail.com>
2021-07-08 02:24:00 -0400

    Solve problem 8

---
+++ solutions/8-string-to-integer.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var myAtoi = function(s) {
+    s = s.trim();
+    let num = 0;
+    let digitIndex = 0;
+    let negate = false;
+    let maxNum = 2 ** 31;
+
+    if (digitIndex < s.length && /\+|-/.test(s[digitIndex])) {
+        negate = s[digitIndex] === "-";
+        digitIndex += 1;
+    }
+
+    while (digitIndex < s.length && /[0-9]/.test(s[digitIndex])) {
+        num = (num * 10) + Number(s[digitIndex]);
+
+        if (negate && num >= maxNum) {
+            num = maxNum;
+            break;
+        } else if (!negate && num >= maxNum - 1) {
+            num = maxNum - 1;
+            break;
+        }
+
+        digitIndex += 1;
+    }
+
+    return (negate ? -num : num);
+
+};
