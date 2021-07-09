ba16996bb9d1ccae4e22b2cae16fb680f047a8c1
Nien Van <nienvan12@gmail.com>
2021-07-08 22:45:40 -0400

    Solve p14 (longest common prefix)

---
+++ solutions/14-longest-common-prefix.js
@@ -0,0 +1,30 @@
+/**
+ * Vertical scanning solution
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    if (strs.length === 1) {
+        return strs[0];
+    }
+
+    let lcp = "";
+    for (let i = 0; i < strs[0].length; i += 1) {
+        let hasCommonChar = true;
+
+        for (let j = 1; j < strs.length; j += 1) {
+            if (strs[j][i] !== strs[0][i]) {
+                hasCommonChar = false;
+                break;
+            }
+        }
+
+        if (hasCommonChar) {
+            lcp += strs[0][i];
+        } else {
+            break;
+        }
+    }
+
+    return lcp;
+};
