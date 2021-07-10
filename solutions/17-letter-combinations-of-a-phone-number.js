b7309429b74e95da95164c42b6c03a953c7b0a4c
Nien Van <nienvan12@gmail.com>
2021-07-09 20:02:39 -0400

    Solve p17 (letter combo of phone num)

---
+++ solutions/17-letter-combinations-of-a-phone-number.js
@@ -0,0 +1,65 @@
+/**
+ * First solution
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+    const dict = {
+        "2": ["a", "b", "c"],
+        "3": ["d", "e", "f"],
+        "4": ["g", "h", "i"],
+        "5": ["j", "k", "l"],
+        "6": ["m", "n", "o"],
+        "7": ["p", "q", "r", "s"],
+        "8": ["t", "u", "v"],
+        "9": ["w", "x", "y", "z"]
+    }
+
+    let ans = [];
+
+    for (let i = 0; i < digits.length; i += 1) {
+        if (ans.length === 0) {
+            ans.push(...dict[digits[i]]);
+            continue;
+        }
+
+        const newAns = [];
+        for (const str of ans) {
+            for (const char of dict[digits[i]]) {
+                newAns.push(str + char);
+            }
+        }
+
+        ans = newAns;
+    }
+
+    return ans;
+};
+
+/**
+ * Second solution: Cleaner version, using a queue
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+    const dict = [ "", "", "abc", "def", "ghi", "jkl", "mno",
+                   "pqrs", "tuv", "wxyz" ];
+
+    let ans = [];
+
+    for (let i = 0; i < digits.length; i += 1) {
+        if (ans.length === 0) {
+            ans.push(...dict[digits[i]]);
+            continue;
+        }
+
+        while (ans[0].length === i) {
+            const curr = ans.shift();
+            for (const char of dict[digits[i]]) {
+                ans.push(curr + char);
+            }
+        }
+    }
+
+    return ans;
+};
