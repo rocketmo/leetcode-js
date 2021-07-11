abf6f0910883e2d8b57822ecc06cf281756b3709
Nien Van <nienvan12@gmail.com>
2021-07-11 18:03:48 -0400

    Solve p937 (reorder log files)

---
+++ solutions/937-reorder-data-in-log-files.js
@@ -0,0 +1,49 @@
+/**
+ * @param {string[]} logs
+ * @return {string[]}
+ */
+var reorderLogFiles = function(logs) {
+    const letId = null;
+    const digitId = null;
+    const originalOrder = new Map();
+
+    for (let i = 0; i < logs.length; i += 1) {
+        originalOrder.set(logs[i], i);
+    }
+
+    logs.sort((a, b) => {
+        const [aId, aWords] = extractWords(a);
+        const [bId, bWords] = extractWords(b);
+
+        const aIsDigit = identify(aWords);
+        const bIsDigit = identify(bWords);
+
+        if (!aIsDigit && bIsDigit) {
+            return -1;
+        } else if (aIsDigit && !bIsDigit) {
+            return 1;
+        } else if (!aIsDigit && !bIsDigit) {
+            if (aWords < bWords) {
+                return -1;
+            } else if (aWords > bWords) {
+                return 1;
+            } else {
+                return aId < bId ? -1 : 1;
+            }
+        }
+
+        return originalOrder.get(a) - originalOrder.get(b);
+    });
+
+    return logs;
+
+    function identify(words) {
+        const split = words.split(" ");
+        return !isNaN(split[0]);
+    }
+
+    function extractWords(log) {
+        const firstSpace = log.indexOf(" ");
+        return [ log.substr(0, firstSpace), log.substr(firstSpace + 1) ];
+    }
+};
