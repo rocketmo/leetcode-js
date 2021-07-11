acc2aa88bcedc5bea60106736d7fcdbdd3ecfd3f
Nien Van <nienvan12@gmail.com>
2021-07-11 19:40:48 -0400

    Solve p1221 (split balanced string)

---
+++ solutions/1221-split-a-string-in-balanced-strings.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var balancedStringSplit = function(s) {
+    let lCount = 0, rCount = 0, numSplits = 0;
+
+    for (let i = 0; i < s.length; i += 1) {
+        if (s[i] === 'L') {
+            lCount += 1;
+        } else {
+            rCount += 1;
+        }
+
+        if (lCount === rCount) {
+            numSplits += 1;
+            lCount = 0;
+            rCount = 0;
+        }
+    }
+
+    return numSplits;
+};
