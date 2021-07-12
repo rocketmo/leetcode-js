d8b9b0e5011a47066a39ebc577951d2c92c46f6d
Nien Van <nienvan12@gmail.com>
2021-07-12 14:49:47 -0400

    Solve p1700 (students at lunch)

---
+++ solutions/1700-number-of-students-unable-to-eat-lunch.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} students
+ * @param {number[]} sandwiches
+ * @return {number}
+ */
+var countStudents = function(students, sandwiches) {
+    let sandwichIndex = 0;
+    const eatenSet = new Set();
+
+    while (sandwichIndex < sandwiches.length) {
+        const originalEatenSize = eatenSet.size;
+
+        for (let i = 0; i < students.length; i += 1) {
+            if (eatenSet.has(i)) { continue; } // student already ate
+
+            if (students[i] === sandwiches[sandwichIndex]) { // student can eat
+                sandwichIndex += 1;
+                eatenSet.add(i);
+            }
+
+            if (sandwichIndex >= sandwiches.length) {
+                break;
+            }
+        }
+
+        if (originalEatenSize === eatenSet.size) { // no students ate during this pass through
+            break;
+        }
+    }
+
+    return students.length - eatenSet.size;
+};
