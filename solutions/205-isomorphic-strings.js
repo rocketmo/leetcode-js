ade80d9b9f49f7d0197e72d6a354bbd03fb466e0
Nien Van <nienvan12@gmail.com>
2021-07-12 14:23:42 -0400

    Solve p205 (isomorphic strings)

---
+++ solutions/205-isomorphic-strings.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    const sCharMap = new Map();
+    const tCharSet = new Set();
+
+    for (let i = 0; i < s.length; i += 1) {
+        const sChar = s[i];
+        const tChar = t[i];
+
+        if ((sCharMap.has(sChar) && sCharMap.get(sChar) !== tChar) ||
+           (!sCharMap.has(sChar) && tCharSet.has(tChar))) {
+            return false;
+        } else if (!sCharMap.has(sChar)) {
+            sCharMap.set(sChar, tChar);
+            tCharSet.add(tChar);
+        }
+    }
+
+    return true;
+};
