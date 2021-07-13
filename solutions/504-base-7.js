fa509a53e925d3cff3e5e732f218be8c60819f68
Nien Van <nienvan12@gmail.com>
2021-07-12 20:37:18 -0400

    Solve p504 (base 7)

---
+++ solutions/504-base-7.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var convertToBase7 = function(num) {
+    if (num === 0) {
+        return "0";
+    }
+
+    let base7 = "";
+    let sign = num < 0 ? "-" : "";
+    let absVal = Math.abs(num);
+
+    while (absVal > 0) {
+        const digit = absVal % 7;
+        absVal = Math.floor(absVal / 7);
+
+        base7 = `${digit}${base7}`;
+    }
+
+
+    return sign + base7;
+};
