0e494e24377efb9cec6fb171f2b1731ec62e7b28
Nien Van <nienvan12@gmail.com>
2021-07-12 20:38:11 -0400

    Solve p1038 (binary to greater sum tree)

---
+++ solutions/1038-binary-search-tree-to-greater-sum-tree.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var bstToGst = function(root) {
+    let gSum = 0;
+    updateSum(root);
+    return root;
+
+    function updateSum (node) {
+        if (!node) {
+            return;
+        }
+
+        updateSum(node.right);
+
+        gSum += node.val;
+        node.val = gSum;
+
+        updateSum(node.left);
+    }
+};
