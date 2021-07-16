4c7af77161e1162482520adab7f88630e9b01e71
Nien Van <nienvan12@gmail.com>
2021-07-16 01:11:10 -0400

    Solve p35 (search insert position)

---
+++ solutions/35-search-insert-position.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let lo = 0, hi = nums.length;
+
+    while (lo < hi) {
+        const mid = Math.floor((lo + hi) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            lo = mid + 1;
+        } else {
+            hi = mid;
+        }
+    }
+
+    return lo;
+};
