ac28bfac5cf867328d07a04a32efb81d2ddb7ab1
Nien Van <nienvan12@gmail.com>
2021-07-18 00:13:19 -0400

    Solve p1935 (max words typed)

---
+++ solutions/1935-maximum-number-of-words-you-can-type.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} text
+ * @param {string} brokenLetters
+ * @return {number}
+ */
+var canBeTypedWords = function(text, brokenLetters) {
+    const words = text.split(" ");
+    const letters = brokenLetters.split("");
+    let numTyped = 0;
+
+    for (const word of words) {
+        let hasLetter = false;
+        for (const letter of letters) {
+            if (word.indexOf(letter) > -1) {
+                hasLetter = true;
+                break;
+            }
+        }
+
+        numTyped += (hasLetter) ? 0 : 1;
+    }
+
+    return numTyped;
+};
