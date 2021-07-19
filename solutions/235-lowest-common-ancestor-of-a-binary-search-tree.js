dcde01c4490c53650727869533b07bd71861bd1e
Nien Van <nienvan12@gmail.com>
2021-07-19 12:57:17 -0400

    Solve p235 (lowest common ancestor bst)

---
+++ solutions/235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+    let currNode = root;
+
+    while (true) {
+        if (p.val < currNode.val && q.val < currNode.val) {
+            currNode = currNode.left;
+        } else if (p.val > currNode.val && q.val > currNode.val) {
+            currNode = currNode.right;
+        } else {
+            break
+        }
+    }
+
+    return currNode;
+};
