9148ea68f51b8da45214dae09f8e8b69b4afb6a9
Nien Van <nienvan12@gmail.com>
2021-07-25 01:24:43 -0400

    Solve p1946 (largest num after mutating substr)

---
+++ solutions/1946-largest-number-after-mutating-substring.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} num
+ * @param {number[]} change
+ * @return {string}
+ */
+var maximumNumber = function(num, change) {
+    let foundSubStr = false;
+    const digits = num.split("");
+
+    for (let i = 0; i < digits.length; i += 1) {
+        const digit = Number(digits[i]);
+
+        if (change[digit] > digit || (change[digit] >= digit && foundSubStr)) {
+            foundSubStr = true;
+            digits[i] = String(change[digit]);
+        } else if (foundSubStr) {
+            break;
+        }
+    }
+
+    return digits.join("");
+};
