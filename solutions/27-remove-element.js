c06388202678561fedde51d3a2b49650582fc5f2
Nien Van <nienvan12@gmail.com>
2021-07-26 00:32:07 -0400

    Solve p27 (remove element)

---
+++ solutions/27-remove-element.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let replaceIndex = 0;
+    let count = 0;
+
+    for (let i = 0; i < nums.length; i += 1) {
+        if (nums[i] === val) {
+            count += 1;
+            continue;
+        }
+
+        nums[replaceIndex] = nums[i];
+        replaceIndex += 1;
+    }
+
+    return nums.length - count;
+};
