99c8887d2d7d749352d95da797715fc2fc6eb6bf
Nien Van <nienvan12@gmail.com>
2021-07-26 23:30:58 -0400

    Solve p23 (merge k sorted lists)

---
+++ solutions/23-merge-k-sorted-lists.js
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function(lists) {
+
+    // Populate min priority queue with the head node of each list
+    const pq = new MinPriorityQueue({ priority: node => node.val });
+
+    for (const node of lists) {
+        if (node) {
+            pq.enqueue(node);
+        }
+    }
+
+    // Add node with lowest value to the new list, add next node to the priority;
+    // continue until the priority queue is empty
+    let newList = null;
+    let currNode = null;
+
+    while (pq.size() > 0) {
+        const node = pq.dequeue().element;
+
+        if (!newList) {
+            newList = node;
+            currNode = newList;
+        } else {
+            currNode.next = node;
+            currNode = node;
+        }
+
+        if (node.next) {
+            pq.enqueue(node.next);
+        }
+    }
+
+    return newList;
+};
