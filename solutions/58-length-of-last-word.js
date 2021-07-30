b51fe9488d631d5d3d63b742923328757bf241a3
Nien Van <nienvan12@gmail.com>
2021-07-30 01:36:37 -0400

    Solve p58 (length of last word)

---
+++ solutions/58-length-of-last-word.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let foundLastWord = false;
+    let len = 0;
+
+    for (let i = s.length - 1; i >= 0; i -= 1) {
+        if (s[i] !== " ") {
+            len += 1;
+            foundLastWord = true;
+        } else if (foundLastWord) {
+            break;
+        }
+    }
+
+    return len;
+};
