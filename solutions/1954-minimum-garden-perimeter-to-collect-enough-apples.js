70bc334ee146e3792e133b310c05f75b6213501e
Nien Van <nienvan12@gmail.com>
2021-08-01 00:23:30 -0400

    Solve p1954 (min garden perimeter)

---
+++ solutions/1954-minimum-garden-perimeter-to-collect-enough-apples.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} neededApples
+ * @return {number}
+ */
+var minimumPerimeter = function(neededApples) {
+    const memo = [ 5 ];
+    let currLen = 1;
+    let currApples = 12;
+
+    while (currApples < neededApples) {
+        const prevPerimeterApples = memo[currLen - 1];
+        currLen += 1;
+
+        const nextPerimeterSide = ((currLen * 2) - 1) + (currLen * 4) + prevPerimeterApples;
+        const nextPerimeterApples = (nextPerimeterSide * 4) - (4 * currLen);
+
+        currApples += nextPerimeterApples;
+        memo.push(nextPerimeterSide);
+    }
+
+    return currLen * 8;
+};
