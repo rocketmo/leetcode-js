a041c9662bc3420f4d572d7434b48decd5d7d6e9
Nien Van <nienvan12@gmail.com>
2021-08-01 00:20:25 -0400

    Solve p1952 (three divisors)

---
+++ solutions/1952-three-divisors.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isThree = function(n) {
+    const divSet = new Set();
+    divSet.add(1);
+    divSet.add(n);
+
+    for (let i = 2; i <= Math.floor(n / 2); i += 1) {
+        const x = Math.floor(n / i);
+
+        if (i * x === n) {
+            divSet.add(i);
+            divSet.add(x);
+        }
+
+        if (divSet.size > 3) {
+            return false;
+        }
+    }
+
+    return divSet.size === 3;
+};
