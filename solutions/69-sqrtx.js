ce5aa796c1bfe7a0f3a4349245169b34ec43210b
Nien Van <nienvan12@gmail.com>
2021-08-01 22:41:17 -0400

    Solve p69 (square root)

---
+++ solutions/69-sqrtx.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    let left = 0;
+    let right = x;
+
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (mid === left) {
+            if (right * right <= x) {
+                left = right;
+            } else {
+                right = left;
+            }
+        } else if (mid * mid > x) {
+            right = mid - 1;
+        } else {
+            left = mid;
+        }
+    }
+
+    return left;
+};
