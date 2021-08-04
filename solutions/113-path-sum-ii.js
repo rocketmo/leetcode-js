40214be9c0adfaf78bb76d39690d0b39945c8ba9
Nien Van <nienvan12@gmail.com>
2021-08-04 15:13:02 -0400

    Solve p113 (path sum ii)

---
+++ solutions/113-path-sum-ii.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {number[][]}
+ */
+var pathSum = function(root, targetSum) {
+    return dfs(root, 0, []);
+
+    function dfs(currNode, currSum, currPath) {
+
+        if (!currNode) {
+            return [];
+        } else if (currSum + currNode.val === targetSum && !currNode.left && !currNode.right) {
+            return [ [ ...currPath, currNode.val ] ];
+        }
+
+        const paths = [];
+
+        if (currNode.left) {
+            paths.push(...dfs(currNode.left, currSum + currNode.val, [ ...currPath, currNode.val] ));
+        }
+
+        if (currNode.right) {
+            paths.push(...dfs(currNode.right, currSum + currNode.val, [ ...currPath, currNode.val] ));
+        }
+
+        return paths;
+    }
+};
