3517e46d600689f6947681a6adbd91d0ca880770
Nien Van <nienvan12@gmail.com>
2021-08-05 16:08:37 -0400

    Solve p1365 (how many smaller nums)

---
+++ solutions/1365-how-many-numbers-are-smaller-than-the-current-number.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var smallerNumbersThanCurrent = function(nums) {
+    const ans = _.fill(Array(nums.length), 0);
+
+    for (let i = 0; i < nums.length - 1; i += 1) {
+        for (let j = i + 1; j < nums.length; j += 1) {
+            if (nums[i] > nums[j]) {
+                ans[i] += 1;
+            } else if (nums[j] > nums[i]) {
+                ans[j] += 1;
+            }
+        }
+    }
+
+    return ans;
+};
