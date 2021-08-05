b12f516a9fbfa4bcefc7ed8295d7d197b60d091e
Nien Van <nienvan12@gmail.com>
2021-08-05 14:13:54 -0400

    Solve p1431 (kid with greatest candies)

---
+++ solutions/1431-kids-with-the-greatest-number-of-candies.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+    const maxCandies = Math.max(...candies);
+    return candies.map(val => (val + extraCandies >= maxCandies));
+};
