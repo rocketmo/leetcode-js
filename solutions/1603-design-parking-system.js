48af820037732042fd9f7fb6be254a8ca35b11da
Nien Van <nienvan12@gmail.com>
2021-08-05 15:27:15 -0400

    Solve p1603 (design parking system)

---
+++ solutions/1603-design-parking-system.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} big
+ * @param {number} medium
+ * @param {number} small
+ */
+var ParkingSystem = function(big, medium, small) {
+    this.spaces = [ big, medium, small ];
+    this.taken = [ 0, 0, 0 ];
+};
+
+/**
+ * @param {number} carType
+ * @return {boolean}
+ */
+ParkingSystem.prototype.addCar = function(carType) {
+    if (this.taken[carType - 1] < this.spaces[carType - 1]) {
+        this.taken[carType - 1] += 1;
+        return true;
+    }
+
+    return false;
+};
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * var obj = new ParkingSystem(big, medium, small)
+ * var param_1 = obj.addCar(carType)
+ */
