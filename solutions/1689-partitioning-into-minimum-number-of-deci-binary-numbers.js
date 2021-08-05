9584f8084839f4c6c724a83c3e9f7b55dac67311
Nien Van <nienvan12@gmail.com>
2021-08-05 14:24:28 -0400

    Solve p1689 (partition into deci-binary nums)

---
+++ solutions/1689-partitioning-into-minimum-number-of-deci-binary-numbers.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} n
+ * @return {number}
+ */
+var minPartitions = function(n) {
+    let partitions = 0;
+
+    for (const digit of n) {
+        partitions = Math.max(partitions, Number(digit));
+
+        if (partitions === 9) {
+            return partitions;
+        }
+    }
+
+    return partitions;
+};
