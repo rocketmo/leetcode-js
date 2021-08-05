7655c95a10b0ffe7238da0904989e0abd6baf44c
Nien Van <nienvan12@gmail.com>
2021-08-05 14:28:05 -0400

    Solve p1512 (number of good pairs)

---
+++ solutions/1512-number-of-good-pairs.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numIdenticalPairs = function(nums) {
+    let numGood = 0;
+
+    for (let i = 0; i < nums.length - 1; i += 1) {
+        for (let j = i + 1; j < nums.length; j += 1) {
+            if (nums[i] === nums[j]) {
+                numGood += 1;
+            }
+        }
+    }
+
+    return numGood;
+};
