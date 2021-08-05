6e68bf8388c6b3ccaa7a389eb3125051beecfd93
Nien Van <nienvan12@gmail.com>
2021-08-04 23:07:32 -0400

    Solve p1480 (running sum of 1d array)

---
+++ solutions/1480-running-sum-of-1d-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+    const runningSums = [];
+    let currSum = 0;
+
+    for (let i = 0; i < nums.length; i += 1) {
+        currSum += nums[i];
+        runningSums.push(currSum);
+    }
+
+    return runningSums;
+};
