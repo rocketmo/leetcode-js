74b3b68686c2ce5b843378b6c4c0430a25cfcc5b
Nien Van <nienvan12@gmail.com>
2021-08-05 14:10:47 -0400

    Solve p1470 (shuffle the array)

---
+++ solutions/1470-shuffle-the-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function(nums, n) {
+    const arr = [];
+
+    for (let i = 0; i < n; i += 1) {
+        arr.push(nums[i]);
+        arr.push(nums[n + i]);
+    }
+
+    return arr;
+};
