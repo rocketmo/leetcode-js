b6d77fb5e1cbd433889094eec1f32d274a6a0563
Nien Van <nienvan12@gmail.com>
2021-08-06 14:35:01 -0400

    Solve p1281 (subtract product and sum of digits)

---
+++ solutions/1281-subtract-the-product-and-sum-of-digits-of-an-integer.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var subtractProductAndSum = function(n) {
+    const digits = n.toString().split("").map(num => Number(num));
+    return digits.reduce((acc, val) => acc * val, 1) - digits.reduce((acc, val) => acc + val, 0);
+};
