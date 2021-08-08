d27a6395f24bbafdc794996cc763dafa266c73c2
Nien Van <nienvan12@gmail.com>
2021-08-08 00:31:42 -0400

    Solve p1961 (string prefix of array)

---
+++ solutions/1961-check-if-string-is-a-prefix-of-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @param {string[]} words
+ * @return {boolean}
+ */
+var isPrefixString = function(s, words) {
+    let word = "";
+    for (let i = 0; i < words.length; i += 1) {
+        word += words[i];
+        if (word === s) {
+            return true;
+        }
+    }
+
+    return false;
+};
