a76f4b177e1554c9814fe39396d0163273e5fac2
Nien Van <nienvan12@gmail.com>
2021-08-08 01:04:48 -0400

    Solve p1964 (longest valid obstacle course)

---
+++ solutions/1964-find-the-longest-valid-obstacle-course-at-each-position.js
@@ -0,0 +1,36 @@
+/**
+ * Finds the longest non-decreasing subsequence
+ * @param {number[]} obstacles
+ * @return {number[]}
+ */
+var longestObstacleCourseAtEachPosition = function(obstacles) {
+    const sequence = [];
+    const ans = [];
+
+    for (let i = 0; i < obstacles.length; i += 1) {
+        const num = obstacles[i];
+
+        if (sequence.length === 0 || num >= sequence[sequence.length - 1]) {
+            sequence.push(num);
+            ans.push(sequence.length);
+        } else {
+            // Binary search to find smallest number greater than num
+            let left = 0;
+            let right = sequence.length;
+
+            while (left < right) {
+                const mid = Math.floor((left + right) / 2);
+                if (sequence[mid] <= num) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+
+            sequence[left] = num;
+            ans.push(left + 1);
+        }
+    }
+
+    return ans;
+};
