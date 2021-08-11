c25e9336e57beb79731f88fe8076426feed2d372
Nien Van <nienvan12@gmail.com>
2021-08-11 12:27:35 -0400

    Solve p1313 (decompress run-length encoded list)

---
+++ solutions/1313-decompress-run-length-encoded-list.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var decompressRLElist = function(nums) {
+    const arr = [];
+
+    for (let i = 0; i < nums.length / 2; i += 1) {
+        const freq = nums[2 * i];
+        const val = nums[(2 * i) + 1];
+
+        for (let j = 0; j < freq; j += 1) {
+            arr.push(val);
+        }
+    }
+
+    return arr;
+};
