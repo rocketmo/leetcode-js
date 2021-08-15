62f12b414ed206bc0ef19a02add3e3006186a145
Nien Van <nienvan12@gmail.com>
2021-08-15 01:06:27 -0400

    Solve p1968 (array not equal to avg neighbours)

---
+++ solutions/1968-array-with-elements-not-equal-to-average-of-neighbors.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var rearrangeArray = function(nums) {
+    nums.sort((a, b) => a - b);
+
+    for (let i = 1; i < nums.length - 1; i += 1) {
+        const avg = (nums[i - 1] + nums[i + 1]) / 2;
+        if (avg === nums[i]) {
+            const tmp = nums[i];
+            nums[i] = nums[i + 1];
+            nums[i + 1] = tmp;
+            i = Math.max(0, i - 2);
+        }
+    }
+
+    return nums;
+};
