ebdcb1e841f5528e590f35601d9a2c92b8a0d544
Nien Van <nienvan12@gmail.com>
2021-08-15 01:04:01 -0400

    Solve p1967 (num strings that appear as substr)

---
+++ solutions/1967-number-of-strings-that-appear-as-substrings-in-word.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string[]} patterns
+ * @param {string} word
+ * @return {number}
+ */
+var numOfStrings = function(patterns, word) {
+    return patterns.reduce((acc, pattern) => {
+        return acc + (word.indexOf(pattern) > -1 ? 1 : 0);
+    }, 0);
+};
