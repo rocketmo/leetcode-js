14bed438272d139a98a0aeda98dd93a52355a476
Nien Van <nienvan12@gmail.com>
2021-08-15 01:02:18 -0400

    Solve p1969 (min non-zero product)

---
+++ solutions/1969-minimum-non-zero-product-of-the-array-elements.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number} p
+ * @return {number}
+ */
+var minNonZeroProduct = function(p) {
+    if (p === 1) {
+        return 1;
+    }
+
+    let ans = (2n ** BigInt(p)) - 1n;
+    const multiplier = ans - 1n;
+    const times = ans / 2n;
+
+    ans = modulo(modulo(ans) * expn(multiplier, times));
+
+    return Number(ans);
+
+
+    function expn(n, k) {
+        if (k === 0n) {
+            return 1n;
+        } else if (k === 1n) {
+            return modulo(n);
+        } else if (k % 2n === 0) {
+            const tmp = expn(n, k / 2n);
+            return modulo(tmp * tmp);
+        } else {
+            let tmp = expn(n, k / 2n);
+            tmp = modulo(tmp * tmp);
+            return modulo(tmp * modulo(n));
+        }
+    }
+
+    function modulo(num) {
+        return num % BigInt(1e9 + 7);
+    }
+};
