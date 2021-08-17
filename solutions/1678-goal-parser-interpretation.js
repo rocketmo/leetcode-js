a8cd20ca6e774537fdd42eb7874d5cff6d34145a
Nien Van <nienvan12@gmail.com>
2021-08-17 13:03:34 -0400

    Solve p1678 (goal parser)

---
+++ solutions/1678-goal-parser-interpretation.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} command
+ * @return {string}
+ */
+var interpret = function(command) {
+    let ans = "";
+
+    for (let i = 0; i < command.length; i += 1) {
+        if (command[i] === "G") {
+            ans += "G";
+        } else if (command[i + 1] === ")") {
+            ans += "o";
+            i += 1;
+        } else {
+            ans += "al";
+            i += 3;
+        }
+    }
+
+    return ans;
+};
