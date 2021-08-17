6167c826641140811f2e96f68cd2eceac5540bf5
Nien Van <nienvan12@gmail.com>
2021-08-17 13:10:15 -0400

    Solve p1379 (find corresponding node in clone)

---
+++ solutions/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} original
+ * @param {TreeNode} cloned
+ * @param {TreeNode} target
+ * @return {TreeNode}
+ */
+var getTargetCopy = function(original, cloned, target) {
+    const stack = [ cloned ];
+
+    while (stack.length) {
+        const node = stack.pop();
+        if (target.val === node.val) {
+            return node;
+        }
+
+        if (node.left) {
+            stack.push(node.left);
+        }
+
+        if (node.right) {
+            stack.push(node.right);
+        }
+    }
+
+    return null;
+};
