8f88ef96b2723228706c2c18394875ec3a1afb57
Nien Van <nienvan12@gmail.com>
2021-08-23 13:33:04 -0400

    Solve p653 (two sum iv - bst)

---
+++ solutions/653-two-sum-iv-input-is-a-bst.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {boolean}
+ */
+var findTarget = function(root, k) {
+    const numSet = new Set();
+    const nodeStack = [ root ];
+
+    while (nodeStack.length) {
+        const currNode = nodeStack.pop();
+        if (!currNode) { continue; }
+
+        const diff = k - currNode.val;
+        if (numSet.has(diff)) {
+            return true;
+        }
+
+        numSet.add(currNode.val);
+        nodeStack.push(currNode.left);
+        nodeStack.push(currNode.right);
+    }
+
+    return false;
+};
