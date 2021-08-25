7a8ca39bf5d3180e7c82c3e295ecc1be051600c3
Nien Van <nienvan12@gmail.com>
2021-08-25 15:13:04 -0400

    Solve p633 (sum of square nums)

---
+++ solutions/633-sum-of-square-numbers.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} c
+ * @return {boolean}
+ */
+var judgeSquareSum = function(c) {
+    const sqrSet = new Set();
+    let i = 0;
+
+    while (i * i <= c) {
+        const sqr = i * i;
+        const diff = c - sqr;
+
+        if (sqrSet.has(diff) || sqr === diff) {
+            return true;
+        }
+
+        sqrSet.add(sqr);
+        i += 1;
+    }
+
+    return false;
+};
