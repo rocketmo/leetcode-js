9bc19b5027dcc6c1dd065c1951c195a13fc1b2eb
Nien Van <nienvan12@gmail.com>
2021-08-28 18:41:21 -0400

    Solve p703 (kth largest element in stream)

---
+++ solutions/703-kth-largest-element-in-a-stream.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number} k
+ * @param {number[]} nums
+ */
+var KthLargest = function(k, nums) {
+    this.k = k;
+    this.nums = nums.sort((a, b) => a - b);
+};
+
+/**
+ * @param {number} val
+ * @return {number}
+ */
+KthLargest.prototype.add = function(val) {
+    let left = 0, right = this.nums.length;
+    while (left < right) {
+        const mid = Math.floor((left + right) / 2);
+        if (val >= this.nums[mid]) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+
+    this.nums.splice(left, 0, val);
+    return this.nums[this.nums.length - this.k];
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * var obj = new KthLargest(k, nums)
+ * var param_1 = obj.add(val)
+ */
