67bdefd3e95ee5a2f00d8dae3232e2ce7686038d
Nien Van <nienvan12@gmail.com>
2021-08-28 19:05:11 -0400

    Solve p344 (reverse string)

---
+++ solutions/344-reverse-string.js
@@ -0,0 +1,27 @@
+/**
+ * This might be cheating...
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    s.reverse();
+};
+
+/**
+ * Another solution, using a pointer
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    let i = 0;
+
+    while (i < s.length - 1 - i) {
+        // Swap left and right characters
+        let right = s.length - 1 - i;
+        let rightVal = s[right];
+        s[right] = s[i];
+        s[i] = rightVal;
+
+        i += 1;
+    }
+};
