479afe344168ff6f0067f729e251a001260b91b7
Nien Van <nienvan12@gmail.com>
2021-08-31 21:11:47 -0400

    Solve p1010 (pairs of songs)

---
+++ solutions/1010-pairs-of-songs-with-total-durations-divisible-by-60.js
@@ -0,0 +1,41 @@
+/**
+ * 1st solution: Use nested loop to check all possible combinations
+ * @param {number[]} time
+ * @return {number}
+ */
+var numPairsDivisibleBy60 = function(time) {
+    let ans = 0;
+    for (let i = 0; i < time.length - 1; i += 1) {
+        for (let j = i + 1; j < time.length; j += 1) {
+            if ((time[i] + time[j]) % 60 === 0) {
+                ans += 1;
+            }
+        }
+    }
+
+    return ans;
+};
+
+/**
+ * 2nd solution: Use a map
+ * @param {number[]} time
+ * @return {number}
+ */
+ var numPairsDivisibleBy60 = function(time) {
+    const diffMap = new Map();
+    let ans = 0;
+
+    for (let i = 0; i < time.length; i += 1) {
+        const diff = time[i] % 60;
+        const diffRequired = diff === 0 ? diff : 60 - diff;
+        ans += diffMap.has(diffRequired) ? diffMap.get(diffRequired) : 0;
+
+        if (!diffMap.has(diff)) {
+            diffMap.set(diff, 0);
+        }
+
+        diffMap.set(diff, diffMap.get(diff) + 1);
+    }
+
+    return ans;
+};
