6da6dd2b33c7658f45afede4dc51790796c961e9
Nien Van <nienvan12@gmail.com>
2021-09-01 23:46:57 -0400

    Solve p136 (single number)

---
+++ solutions/136-single-number.js
@@ -0,0 +1,33 @@
+/**
+ * First solution: Uses a set, O(n) space
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    const numSet = new Set();
+
+    for (const num of nums) {
+        if (numSet.has(num)) {
+            numSet.delete(num);
+        } else {
+            numSet.add(num);
+        }
+    }
+
+    for (const val of numSet.values()) {
+        return val;
+    }
+};
+
+/**
+ * Second solution: Bit manipulation, constant space
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    for (let i = 1; i < nums.length; i += 1) {
+        nums[0] ^= nums[i];
+    }
+
+    return nums[0];
+};
