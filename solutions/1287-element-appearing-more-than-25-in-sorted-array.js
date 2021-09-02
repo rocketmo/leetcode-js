869e08cf7b01c50b6857c116a79766a2f3fedc8a
Nien Van <nienvan12@gmail.com>
2021-09-01 23:04:13 -0400

    Solve p1287 (element appearing more than 25%)

---
+++ solutions/1287-element-appearing-more-than-25-in-sorted-array.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var findSpecialInteger = function(arr) {
+    const threshold = Math.floor((arr.length / 4));
+    let currCount = 0;
+    let currNum = -1;
+
+    for (let i = 0; i < arr.length; i += 1) {
+        if (arr[i] === currNum) {
+            currCount += 1;
+        } else {
+            currNum = arr[i];
+            currCount = 1;
+        }
+
+        if (currCount > threshold) {
+            return currNum;
+        }
+    }
+
+    return null;
+
+};
