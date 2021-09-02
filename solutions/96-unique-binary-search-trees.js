35073ef99a9455ffc711144a368f0d3ca99bf5e8
Nien Van <nienvan12@gmail.com>
2021-09-02 13:22:57 -0400

    Solve p96 (unique bst)

---
+++ solutions/96-unique-binary-search-trees.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numTrees = function(n) {
+    const ans = [ 1 ];
+
+    while (ans.length < n) {
+        let total = 0;
+        let next = ans.length + 1;
+
+        for (let i = 0; i < next; i += 1) {
+            let leftNodes = i;
+            let rightNodes = next - i - 1;
+
+            let leftMult = leftNodes === 0 ? 1 : ans[leftNodes - 1];
+            let rightMult = rightNodes === 0 ? 1 : ans[rightNodes - 1];
+
+            total += (leftMult * rightMult);
+        }
+
+        ans.push(total);
+    }
+
+    return ans[n - 1];
+};
