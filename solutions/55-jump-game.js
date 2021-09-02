fb344600dd95f585ec45ca4feea3c53b182d61c1
Nien Van <nienvan12@gmail.com>
2021-09-02 01:36:23 -0400

    Solve p55 (jump game)

---
+++ solutions/55-jump-game.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    let maxIdx = 0;
+
+    for (let i = 0; i < nums.length && i <= maxIdx; i += 1) {
+        maxIdx = Math.max(maxIdx, i + nums[i]);
+
+        if (maxIdx >= nums.length - 1) {
+            break;
+        }
+    }
+
+    return maxIdx >= nums.length - 1;
+};
