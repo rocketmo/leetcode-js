d91ace719f0144ae98b6e77c158111baf5de816d
Nien Van <nienvan12@gmail.com>
2021-09-05 01:21:24 -0400

    Solve p1997 (first day in all rooms)

---
+++ solutions/1997-first-day-where-you-have-been-in-all-the-rooms.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nextVisit
+ * @return {number}
+ */
+var firstDayBeenInAllRooms = function(nextVisit) {
+    const n = nextVisit.length;
+    const mod = 1e9 + 7;
+    const roomVisits = _.fill(Array(n), 0);
+
+    for (let i = 0; i < n - 1; i += 1) {
+        const jumpVisits = (2 + roomVisits[i] - roomVisits[nextVisit[i]] + mod) % mod;
+        roomVisits[i + 1] = (roomVisits[i] + jumpVisits) % mod;
+    }
+
+    return roomVisits[n - 1];
+};
