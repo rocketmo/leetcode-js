482845547c4ec2d1395e2840eea119113797863e
Nien Van <nienvan12@gmail.com>
2021-09-06 14:52:53 -0400

    Solve p1629 (slowest key)

---
+++ solutions/1629-slowest-key.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} releaseTimes
+ * @param {string} keysPressed
+ * @return {character}
+ */
+var slowestKey = function(releaseTimes, keysPressed) {
+    let longestKey = keysPressed[0];
+    let longestTime = releaseTimes[0];
+
+    for (let i = 1; i < releaseTimes.length; i += 1) {
+        let currTime = releaseTimes[i] - releaseTimes[i - 1];
+        let currKey = keysPressed[i];
+
+        if (currTime > longestTime || (currTime === longestTime && currKey > longestKey)) {
+            longestKey = currKey;
+            longestTime = currTime;
+        }
+    }
+
+    return longestKey;
+};
