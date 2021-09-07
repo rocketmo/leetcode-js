c5e597c0fd6962dfe3a3e6a40f0298a3f61bd214
Nien Van <nienvan12@gmail.com>
2021-09-07 14:26:26 -0400

    Solve p206 (reverse linked list)

---
+++ solutions/206-reverse-linked-list.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    if (!head) {
+        return head;
+    }
+
+    let currHead = head;
+    let currNode = head.next;
+    currHead.next = null;
+
+    while (currNode) {
+        let tmp = currNode.next;
+        currNode.next = currHead;
+        currHead = currNode;
+        currNode = tmp;
+    }
+
+    return currHead;
+};
