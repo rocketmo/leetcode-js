85d6cf6f6ef91b21015f35c23d6ee1838c37231e
Nien Van <nienvan12@gmail.com>
2021-09-08 17:21:55 -0400

    Solve p848 (shifting letters)

---
+++ solutions/848-shifting-letters.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @param {number[]} shifts
+ * @return {string}
+ */
+var shiftingLetters = function(s, shifts) {
+    const minCharCode = "a".charCodeAt(0);
+    const maxCharCode = "z".charCodeAt(0);
+    const n = shifts.length;
+    const shiftsByCharPos = _.fill(Array(n), 0);
+    shiftsByCharPos[n - 1] = shifts[n - 1] % 26;
+
+    for (let i = n - 2; i >= 0; i -= 1) {
+        shiftsByCharPos[i] = (shifts[i] + shiftsByCharPos[i + 1]) % 26;
+    }
+
+    return s.split("").map((char, idx) => {
+        let currCharCode = char.charCodeAt(0);
+        let shiftedCharCode = currCharCode + shiftsByCharPos[idx];
+        if (shiftedCharCode > maxCharCode) {
+            shiftedCharCode = (shiftedCharCode % (maxCharCode + 1)) + minCharCode;
+        }
+
+        return String.fromCharCode(shiftedCharCode);
+    }).join("");
+};
