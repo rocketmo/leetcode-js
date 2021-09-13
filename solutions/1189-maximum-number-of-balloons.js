cda2daf19349b377840df89139d5682967a3a019
Nien Van <nienvan12@gmail.com>
2021-09-13 18:02:43 -0400

    Solve p1189 (max num balloons)

---
+++ solutions/1189-maximum-number-of-balloons.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} text
+ * @return {number}
+ */
+var maxNumberOfBalloons = function(text) {
+    const charCount = {
+        b: 0,
+        a: 0,
+        l: 0,
+        o: 0,
+        n: 0
+    };
+
+    for (const char of text) {
+        if (charCount[char] !== undefined) {
+            charCount[char] += 1;
+        }
+    }
+
+    return Math.min(charCount.b, charCount.a, Math.floor(charCount.l / 2),
+        Math.floor(charCount.o / 2), charCount.n);
+};
