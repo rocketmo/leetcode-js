8d6f20c1890c3a5321f812dec29b4df5cc419abc
Nien Van <nienvan12@gmail.com>
2021-09-15 16:51:10 -0400

    Solve p978 (longest turbulent subarray)

---
+++ solutions/978-longest-turbulent-subarray.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var maxTurbulenceSize = function(arr) {
+    let over = true;
+    let maxLen = 1;
+    let currLen = 1;
+    let currNum = arr[0];
+
+    for (let i = 1; i < arr.length; i += 1) {
+        if ((over && arr[i] > currNum) || (!over && arr[i] < currNum)) {
+            over = !over;
+            currLen += 1;
+            maxLen = Math.max(maxLen, currLen);
+        } else if (arr[i] !== currNum) {
+            currLen = 2;
+            maxLen = Math.max(maxLen, currLen);
+        } else {
+            currLen = 1;
+        }
+
+        currNum = arr[i];
+    }
+
+    return maxLen;
+};
