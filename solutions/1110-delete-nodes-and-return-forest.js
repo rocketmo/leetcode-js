b75ba15ef85e273d01f9d983b8bb3a81d8ca7d1d
Nien Van <nienvan12@gmail.com>
2021-09-16 13:51:56 -0400

    Solve p1110 (delete nodes and return forest)

---
+++ solutions/1110-delete-nodes-and-return-forest.js
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number[]} to_delete
+ * @return {TreeNode[]}
+ */
+var delNodes = function(root, to_delete) {
+    const ans = [];
+    const deleteSet = new Set(to_delete);
+    helper(root, null);
+    return ans;
+
+    function helper(node, parent, isLeft) {
+        if (!node) {
+            return;
+        }
+
+        if (deleteSet.has(node.val)) {
+            if (parent) {
+                if (isLeft) {
+                    parent.left = null;
+                } else {
+                    parent.right = null;
+                }
+            }
+
+            helper(node.left, null);
+            helper(node.right, null);
+        } else {
+            if (!parent) {
+                ans.push(node);
+            }
+
+            helper(node.left, node, true);
+            helper(node.right, node, false);
+        }
+    }
+};
