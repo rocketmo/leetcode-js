768d41b93ea344a9a86691cbc9983feb0b119dd2
Nien Van <nienvan12@gmail.com>
2021-09-17 15:02:35 -0400

    Solve p349 (intersection of two arrays)

---
+++ solutions/349-intersection-of-two-arrays.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    const numSet1 = new Set(nums1);
+    const numSet2 = new Set();
+    const ans = [];
+
+    for (const num of nums2) {
+        if (numSet1.has(num) && !numSet2.has(num)) {
+            ans.push(num);
+            numSet2.add(num);
+        }
+    }
+
+    return ans;
+};
