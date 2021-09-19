32bd3bd1a1d3a7c842c51edc43ccd259a9c8f3ae
Nien Van <nienvan12@gmail.com>
2021-09-19 00:23:25 -0400

    Solve p2011 (final value after performing ops)

---
+++ solutions/2011-final-value-of-variable-after-performing-operations.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string[]} operations
+ * @return {number}
+ */
+var finalValueAfterOperations = function(operations) {
+    const ops = {
+        "++X": 1,
+        "X++": 1,
+        "--X": -1,
+        "X--": -1,
+    };
+
+    let ans = 0;
+
+    for (const op of operations) {
+        if (ops[op]) {
+            ans += ops[op];
+        }
+    }
+
+    return ans;
+};
