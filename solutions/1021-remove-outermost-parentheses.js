b3c9a5b58f41998328c9bc675c011615041bf3a2
Nien Van <nienvan12@gmail.com>
2021-09-20 23:29:04 -0400

    Solve p1021 (remove outermost parentheses)

---
+++ solutions/1021-remove-outermost-parentheses.js
@@ -0,0 +1,31 @@
+const assert = require("assert");
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeOuterParentheses = function(s) {
+    let str = "";
+    let lCount = 0;
+    let rCount = 0;
+    let lIdx = 0;
+
+    for (let i = 0; i < s.length; i += 1) {
+        if (s[i] === "(") {
+            lCount += 1;
+        } else {
+            rCount += 1;
+        }
+
+        if (lCount === rCount) {
+            str += s.slice(lIdx + 1, i);
+            lIdx = i + 1;
+        }
+    }
+
+    return str;
+};
+
+assert.equal(removeOuterParentheses("(()())(())"), "()()()");
+assert.equal(removeOuterParentheses("(()())(())(()(()))"), "()()()()(())");
+assert.equal(removeOuterParentheses("()()"), "");
