52557cbb675afb1fe023e399d245b66078dff25c
Nien Van <nienvan12@gmail.com>
2021-09-20 21:04:29 -0400

    Solve p1656 (design ordered stream)

---
+++ solutions/1656-design-an-ordered-stream.js
@@ -0,0 +1,39 @@
+const assert = require("assert");
+
+/**
+ * @param {number} n
+ */
+var OrderedStream = function(n) {
+    this.stream = Array(n);
+    this.idx = 0;
+};
+
+/**
+ * @param {number} idKey
+ * @param {string} value
+ * @return {string[]}
+ */
+OrderedStream.prototype.insert = function(idKey, value) {
+    const ret = [];
+    this.stream[idKey - 1] = value;
+
+    while (this.stream[this.idx]) {
+        ret.push(this.stream[this.idx]);
+        this.idx += 1;
+    }
+
+    return ret;
+};
+
+/**
+ * Your OrderedStream object will be instantiated and called as such:
+ * var obj = new OrderedStream(n)
+ * var param_1 = obj.insert(idKey,value)
+ */
+
+const os = new OrderedStream(5);
+assert.deepEqual(os.insert(3, "ccccc"), []);
+assert.deepEqual(os.insert(1, "aaaaa"), ["aaaaa"]);
+assert.deepEqual(os.insert(2, "bbbbb"), ["bbbbb", "ccccc"]);
+assert.deepEqual(os.insert(5, "eeeee"), []);
+assert.deepEqual(os.insert(4, "ddddd"), ["ddddd", "eeeee"]);
