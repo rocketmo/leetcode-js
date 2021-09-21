7a5f4af3132f4dbf298c5da7b0068b86019eb278
Nien Van <nienvan12@gmail.com>
2021-09-20 21:44:18 -0400

    Solve p1588 (sum of all odd length subarrays)

---
+++ solutions/1588-sum-of-all-odd-length-subarrays.js
@@ -0,0 +1,35 @@
+const assert = require("assert");
+
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var sumOddLengthSubarrays = function(arr) {
+    let ans = 0;
+
+    for (let i = 0; i < arr.length; i += 1) {
+        ans += arr[i];
+        
+        let currSum = arr[i];
+        let count = 0;
+        let currIdx = i + 1;
+
+        while (currIdx < arr.length) {
+            currSum += arr[currIdx];
+            count += 1;
+
+            if (count === 2) {
+                ans += currSum;
+                count = 0;
+            }
+
+            currIdx += 1;
+        }
+    }
+
+    return ans;
+};
+
+assert.equal(sumOddLengthSubarrays([1,4,2,5,3]), 58);
+assert.equal(sumOddLengthSubarrays([1,2]), 3);
+assert.equal(sumOddLengthSubarrays([10,11,12]), 66);
