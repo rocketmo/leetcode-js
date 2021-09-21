880e76a8af09ff603498ccd59933c1076b7b8263
Nien Van <nienvan12@gmail.com>
2021-09-21 17:09:43 -0400

    Solve p485 (max consecutive ones)

---
+++ solutions/485-max-consecutive-one.js
@@ -0,0 +1,24 @@
+const assert = require("assert");
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function(nums) {
+    let ans = 0;
+    let currAns = 0;
+
+    for (const num of nums) {
+        if (num === 1) {
+            currAns += 1;
+            ans = Math.max(ans, currAns);
+        } else {
+            currAns = 0;
+        }
+    }
+
+    return ans;
+};
+
+assert.equal(findMaxConsecutiveOnes([1,1,0,1,1,1]), 3);
+assert.equal(findMaxConsecutiveOnes([1,0,1,1,0,1]), 2);
