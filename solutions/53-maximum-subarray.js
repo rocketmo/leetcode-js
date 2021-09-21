b3338ce7ef2bd1371f175f72f0966a1388cbf7c8
Nien Van <nienvan12@gmail.com>
2021-09-21 00:18:52 -0400

    Solve p53 (max subarray)

---
+++ solutions/53-maximum-subarray.js
@@ -0,0 +1,22 @@
+const assert = require("assert");
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let ans = nums[0];
+    const dp = [ ans ];
+
+    for (let i = 1; i < nums.length; i += 1) {
+        const curr = Math.max(nums[i], nums[i] + dp[i - 1]);
+        ans = Math.max(ans, curr);
+        dp.push(curr);
+    }
+
+    return ans;
+};
+
+assert.equal(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]), 6);
+assert.equal(maxSubArray([1]), 1);
+assert.equal(maxSubArray([5,4,-1,7,8]), 23);
