3f074f1520ac995ce49a4247df47b0e407c032b3
Nien Van <nienvan12@gmail.com>
2021-09-22 02:18:30 -0400

    Solve p1382 (balance a bst)

---
+++ solutions/1382-balance-a-binary-search-tree.js
@@ -0,0 +1,42 @@
+const { TreeNode } = require("../other/definitions");
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var balanceBST = function(root) {
+    const ordered = [];
+    inOrderTraversal(root);
+    return constructBalancedTree(ordered);
+
+    function inOrderTraversal(node) {
+        if (!node) {
+            return;
+        }
+
+        inOrderTraversal(node.left);
+        ordered.push(node.val);
+        inOrderTraversal(node.right);
+    }
+
+    function constructBalancedTree(arr) {
+        if (!arr || !arr.length) {
+            return null;
+        }
+
+        const mid = Math.floor(arr.length / 2);
+        const root = new TreeNode(arr[mid]);
+        root.left = constructBalancedTree(arr.slice(0, mid));
+        root.right = constructBalancedTree(arr.slice(mid + 1));
+
+        return root;
+    }
+};
