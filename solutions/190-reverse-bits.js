1d9c15adc386bfcbef1e233caa531a54f99a6061
Nien Van <nienvan12@gmail.com>
2021-09-21 20:22:19 -0400

    Solve p190 (reverse bits)

---
+++ solutions/190-reverse-bits.js
@@ -0,0 +1,17 @@
+const assert = require("assert");
+
+/**
+ * @param {number} n - a positive integer
+ * @return {number} - a positive integer
+ */
+var reverseBits = function(n) {
+    let bin = n.toString(2).split("").reverse().join("");
+    while (bin.length < 32) {
+        bin += "0";
+    }
+
+    return parseInt(bin, 2);
+};
+
+assert.equal(reverseBits(43261596), 964176192);
+assert.equal(reverseBits(4294967293), 3221225471 );
