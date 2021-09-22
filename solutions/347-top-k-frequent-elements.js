8887ea3eaf698fa100ee2dc9b1b16cbe91adb121
Nien Van <nienvan12@gmail.com>
2021-09-22 01:19:12 -0400

    Solve p347 (top k frequent elements)

---
+++ solutions/347-top-k-frequent-elements.js
@@ -0,0 +1,32 @@
+const assert = require("assert");
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    const countMap = new Map();
+
+    for (const num of nums) {
+        if (countMap.has(num)) {
+            countMap.set(num, countMap.get(num) + 1);
+        } else {
+            countMap.set(num, 1);
+        }
+    }
+
+    const arr = [];
+    for (const key of countMap.keys()) {
+        arr.push({ num: key, count: countMap.get(key) });
+    }
+
+    arr.sort((a, b) => {
+        return b.count - a.count;
+    });
+
+    return arr.slice(0, k).map(obj => obj.num);
+};
+
+assert.deepEqual(topKFrequent([1,1,1,2,2,3], 2), [1,2]);
+assert.deepEqual(topKFrequent([1], 1), [1]);
