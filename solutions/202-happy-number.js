eb77ed16c7647a6fe4c7707440ce7a566e198a9b
Nien Van <nienvan12@gmail.com>
2021-09-21 20:51:34 -0400

    Solve p202 (happy number)

---
+++ solutions/202-happy-number.js
@@ -0,0 +1,32 @@
+const assert = require("assert");
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function(n) {
+    let prev = n;
+    let seenSet = new Set();
+
+    while (true) {
+        const digits = prev.toString(10).split("").map(digit => {
+            return parseInt(digit);
+        });
+
+        const sum = digits.reduce((acc, val) => {
+            return acc + (val * val);
+        }, 0);
+
+        prev = sum;
+        if (seenSet.has(sum) || sum === 1) {
+            break;
+        }
+
+        seenSet.add(sum);
+    }
+
+    return prev === 1;
+};
+
+assert(isHappy(19));
+assert.equal(isHappy(2), false);
