bfd9e3c0b213e5e2d824ee7e901e23cf3092dc96
Nien Van <nienvan12@gmail.com>
2021-09-21 20:15:04 -0400

    Solve p172 (factorial trailing zeroes)

---
+++ solutions/172-factorial-trailing-zeroes.js
@@ -0,0 +1,18 @@
+const assert = require("assert");
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var trailingZeroes = function(n) {
+    if (n === 0) {
+        return 0;
+    }
+
+    return Math.floor(n / 5) + trailingZeroes(Math.floor(n / 5));
+};
+
+assert.equal(trailingZeroes(3), 0);
+assert.equal(trailingZeroes(5), 1);
+assert.equal(trailingZeroes(0), 0);
+assert.equal(trailingZeroes(25), 6);
