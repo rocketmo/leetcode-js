f25f60a50cac36ceca87f30ad2f008e6c97ea886
Nien Van <nienvan12@gmail.com>
2021-09-23 01:07:45 -0400

    Solve p1572 (matrix diagonal sum)

---
+++ solutions/1572-matrix-diagonal-sum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[][]} mat
+ * @return {number}
+ */
+var diagonalSum = function(mat) {
+    let ans = 0;
+    for (let i = 0; i < mat.length; i += 1) {
+        ans += mat[i][i];
+
+        const second = mat.length - 1 - i;
+        if (second !== i) {
+            ans += mat[i][second];
+        }
+    }
+
+    return ans;
+};
