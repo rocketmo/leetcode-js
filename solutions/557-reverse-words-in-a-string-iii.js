20ac89d7384e26a4aa6299a03e3bf1004a675eef
Nien Van <nienvan12@gmail.com>
2021-09-23 23:38:25 -0400

    Solve p557 (reverse words in string iii)

---
+++ solutions/557-reverse-words-in-a-string-iii.js
@@ -0,0 +1,14 @@
+const assert = require("assert");
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    return s.split(" ").map(word => {
+        return word.split("").reverse().join("");
+    }).join(" ");
+};
+
+assert.equal(reverseWords("Let's take LeetCode contest"), "s'teL ekat edoCteeL tsetnoc");
+assert.equal(reverseWords("God Ding"), "doG gniD");
