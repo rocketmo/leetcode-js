d1079226e3870cb967375c92dc097db79d94bba8
Nien Van <nienvan12@gmail.com>
2021-09-28 13:55:36 -0400

    Solve p922 (sort array by parity ii)

---
+++ solutions/922-sort-array-by-parity-ii.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParityII = function(nums) {
+    let oddIdx = 1;
+
+    for (let i = 0; i < nums.length; i += 2) {
+        if (nums[i] % 2 === 1) {
+            while (oddIdx < nums.length) {
+                if (nums[oddIdx] % 2 === 0) {
+                    const tmp = nums[i];
+                    nums[i] = nums[oddIdx];
+                    nums[oddIdx] = tmp;
+
+                    oddIdx += 2;
+                    break;
+                }
+
+                oddIdx += 2;
+            }
+        }
+    }
+
+    return nums;
+};
