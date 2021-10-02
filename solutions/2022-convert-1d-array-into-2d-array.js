f5130aa9760006225919a19c917b78792e2cf763
Nien Van <nienvan12@gmail.com>
2021-10-02 12:09:19 -0400

    Solve problems from biweekly contest 62

---
+++ solutions/2022-convert-1d-array-into-2d-array.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} original
+ * @param {number} m
+ * @param {number} n
+ * @return {number[][]}
+ */
+var construct2DArray = function(original, m, n) {
+    if (original.length !== (m * n)) {
+        return [];
+    }
+
+    const ans = [];
+    let idx = 0;
+
+    for (let i = 0; i < m; i += 1) {
+        ans.push([]);
+        for (let j = 0; j < n; j += 1) {
+            ans[i].push(original[idx]);
+            idx += 1;
+        }
+    }
+
+    return ans;
+};
---
+++ solutions/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string[]} nums
+ * @param {string} target
+ * @return {number}
+ */
+var numOfPairs = function(nums, target) {
+    let ans = 0;
+
+    for (let i = 0; i < nums.length; i += 1) {
+        for (let j = 0; j < nums.length; j += 1) {
+            if (i === j || nums[i].length + nums[j].length !== target.length) {
+                continue;
+            } else if (nums[i] + nums[j] === target) {
+                ans += 1;
+            }
+        }
+    }
+
+    return ans;
+};
