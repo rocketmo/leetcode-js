b7adcd80d2935374efb8d999184b95a7c14ed24d
Nien Van <nienvan12@gmail.com>
2021-10-04 20:58:55 -0400

    Solve p203 (removed linked list elements)

---
+++ solutions/203-remove-linked-list-elements.js
@@ -0,0 +1,33 @@
+const { ListNode } = require("../other/definitions");
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+    const tmpHead = new ListNode();
+    tmpHead.next = head;
+
+    let prev = tmpHead;
+    let curr = head;
+
+    while (curr) {
+        if (curr.val === val) {
+            prev.next = curr.next;
+            curr = curr.next;
+        } else {
+            prev = curr;
+            curr = curr.next;
+        }
+    }
+
+    return tmpHead.next;
+};
