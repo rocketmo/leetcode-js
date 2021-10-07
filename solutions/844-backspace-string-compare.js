527700dec94db1f1e4867524962cef84e0bd128f
Nien Van <nienvan12@gmail.com>
2021-10-06 21:33:20 -0400

    Solve p844 (backspace string compare)

---
+++ solutions/844-backspace-string-compare.js
@@ -0,0 +1,34 @@
+const assert = require("assert");
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var backspaceCompare = function(s, t) {
+    const bsStr1 = getBackspaceString(s);
+    const bsStr2 = getBackspaceString(t);
+
+    return bsStr1 === bsStr2;
+
+    function getBackspaceString(str) {
+        let bsStr = [];
+
+        for (const char of str) {
+            if (char === "#") {
+                if (bsStr.length > 0) {
+                    bsStr.pop();
+                }
+            } else {
+                bsStr.push(char);
+            }
+        }
+
+        return bsStr.join("");
+    }
+};
+
+assert(backspaceCompare("ab#c", "ad#c"));
+assert(backspaceCompare("ab##", "c#d#"));
+assert(backspaceCompare("a##c", "#a#c"));
+assert.equal(backspaceCompare("a#c", "b"), false);
