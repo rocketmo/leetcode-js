38d88846c9e570792e8a578b34b71e6453e844d8
Nien Van <nienvan12@gmail.com>
2021-10-15 23:57:45 -0400

    Solve p832 (flipping an image)

---
+++ solutions/832-flipping-an-image.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[][]} image
+ * @return {number[][]}
+ */
+var flipAndInvertImage = function(image) {
+    for (let i = 0; i < image.length; i += 1) {
+        let left = 0;
+        let right = image[i].length - 1;
+
+        while (left <= right) {
+            if (left === right) {
+                image[i][left] = invert(image[i][left]);
+            } else {
+                const tmp = image[i][left];
+                image[i][left] = invert(image[i][right]);
+                image[i][right] = invert(tmp);
+            }
+
+            left += 1;
+            right -= 1;
+        }
+    }
+
+    return image;
+
+    function invert(num) {
+        if (num === 0) {
+            return 1;
+        }
+
+        return 0;
+    }
+};
