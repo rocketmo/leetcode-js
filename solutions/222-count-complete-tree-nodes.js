b9384cb609dae50c2c0e094bfbf9f3ed236f5057
Nien Van <nienvan12@gmail.com>
2021-10-24 00:54:45 -0400

    Solve p222 (count complete tree nodes)

---
+++ solutions/222-count-complete-tree-nodes.js
@@ -0,0 +1,45 @@
+const assert = require("assert");
+const { convertArrayToBinaryTree } = require("../other/util");
+
+function getHeight(root) {
+    let height = 0;
+
+    while (root) {
+        root = root.left;
+        height += 1;
+    }
+
+    return height;
+}
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var countNodes = function(root) {
+    if (!root) {
+        return 0;
+    }
+
+    const leftHeight = getHeight(root.left);
+    const rightHeight = getHeight(root.right);
+
+    if (leftHeight === rightHeight) {
+        return (2 ** leftHeight) + countNodes(root.right);
+    }
+
+    return (2 ** rightHeight) + countNodes(root.left);
+};
+
+assert.equal(countNodes(convertArrayToBinaryTree([1,2,3,4,5,6])), 6);
+assert.equal(countNodes(convertArrayToBinaryTree([])), 0);
+assert.equal(countNodes(convertArrayToBinaryTree([1])), 1);
+assert.equal(countNodes(convertArrayToBinaryTree([1,2,3,4])), 4);
