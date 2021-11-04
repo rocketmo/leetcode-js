a813ea0f63fc4357e1393751c4c99dd2839b4213
Nien Van <nienvan12@gmail.com>
2021-11-03 22:25:32 -0400

    Solve p404 (sum of left leaves)

---
+++ solutions/404-sum-of-left-leaves.js
@@ -0,0 +1,37 @@
+const assert = require("assert");
+const { convertArrayToBinaryTree } = require("../other/util");
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumOfLeftLeaves = function(root) {
+    let sum = 0;
+    helper(root);
+    return sum;
+
+    function helper(node, isLeft) {
+        if (!node) {
+            return;
+        }
+
+        if (!node.left && !node.right) {
+            sum += isLeft ? node.val : 0;
+            return;
+        }
+
+        helper(node.left, true);
+        helper(node.right, false);
+    }
+};
+
+assert.equal(sumOfLeftLeaves(convertArrayToBinaryTree([3,9,20,null,null,15,7])), 24);
+assert.equal(sumOfLeftLeaves(convertArrayToBinaryTree([1])), 0);
