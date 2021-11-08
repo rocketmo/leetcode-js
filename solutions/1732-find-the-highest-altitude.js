ed016b3569f643336eb959825c5d734a28d48d46
Nien Van <nienvan12@gmail.com>
2021-11-07 23:11:33 -0500

    Solve p1732 (find the highest altitude)

---
+++ solutions/1732-find-the-highest-altitude.js
@@ -0,0 +1,20 @@
+const assert = require("assert");
+
+/**
+ * @param {number[]} gain
+ * @return {number}
+ */
+var largestAltitude = function(gain) {
+    let highest = 0;
+    let curr = 0;
+
+    for (const diff of gain) {
+        curr += diff;
+        highest = Math.max(curr, highest);
+    }
+
+    return highest;
+};
+
+assert.equal(largestAltitude([-5,1,5,0,-7]), 1);
+assert.equal(largestAltitude([-4,-3,-2,-1,4,3,2]), 0);
