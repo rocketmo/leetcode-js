5dafe1314737b059e7f603954cd1e28787b4d9ec
Nien Van <nienvan12@gmail.com>
2021-11-10 23:46:11 -0500

    Solve p1413 (min val to get positive step sum)

---
+++ solutions/1413-minimum-value-to-get-positive-step-by-step-sum.js
@@ -0,0 +1,21 @@
+const assert = require("assert");
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minStartValue = function(nums) {
+    let min = 0;
+    let total = 0;
+
+    for (const num of nums) {
+        total += num;
+        min = Math.min(min, total);
+    }
+
+    return -(min - 1);
+};
+
+assert.equal(minStartValue([-3,2,-3,4,2]), 5);
+assert.equal(minStartValue([1,2]), 1);
+assert.equal(minStartValue([1,-2,-3]), 5);
