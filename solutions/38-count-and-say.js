3a846c622558cba117a5aa268f88df7e1976baf1
Nien Van <nienvan12@gmail.com>
2022-05-14 22:34:31 -0400

    Solve p38 (count and say)

---
+++ solutions/38-count-and-say.js
@@ -0,0 +1,44 @@
+const assert = require("assert");
+
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var countAndSay = function(n) {
+    let ans = "1";
+
+    for (let i = 1; i < n; i += 1) {
+        const groups = groupDigits(ans);
+        let newAns = "";
+
+        for (const group of groups) {
+            newAns += group.count;
+            newAns += group.digit;
+        }
+
+        ans = newAns;
+    }
+
+    return ans;
+
+    function groupDigits(value) {
+        const groups = [];
+
+        for (let digit of value) {
+            if (groups.length === 0 || groups[groups.length - 1].digit !== digit) {
+                groups.push({ digit, count: 1});
+            } else {
+                groups[groups.length -1].count += 1;
+            }
+        }
+
+        return groups;
+    }
+};
+
+assert.equal(countAndSay(1), "1");
+assert.equal(countAndSay(2), "11");
+assert.equal(countAndSay(3), "21");
+assert.equal(countAndSay(4), "1211");
+assert.equal(countAndSay(5), "111221");
+assert.equal(countAndSay(6), "312211");
