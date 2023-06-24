5cf0ae590967d96a555ccbc7b6fd44b5fdd2f8d4
Nien Van <nienvan12@gmail.com>
2023-06-24 13:20:53 -0400

    Solve p2619 (array prototype last)

---
+++ solutions/2619-array-prototype-last.js
@@ -0,0 +1,15 @@
+const assert = require("assert");
+
+Array.prototype.last = function() {
+    if (this.length) {
+        return this[this.length - 1];
+    }
+
+    return -1;
+};
+
+const arr1 = [1, 2, 3];
+const arr2 = [];
+
+assert.equal(arr1.last(), 3);
+assert.equal(arr2.last(), -1);
